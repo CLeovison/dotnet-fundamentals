5cdbd24eaac8e78bd2bb2079902b9a6f3a98c873
Clark Leovison Rey <cleovison@gmail.com>
2024-12-30 21:49:05 +0800

    feat: Done understanding of sorting and searching

---
+++ Part 10 - Sorting and Searching Lists/Program.cs
@@ -0,0 +1,18 @@
+﻿var numbers = new List<int> {1,2,4,7,5,6,3};
+
+
+
+
+Console.WriteLine($"{numbers.IndexOf(5)}");
+ 
+
+//If you are using an array, instead of using for each or string or any type you can use List Generics 
+//To Sort all the current numbers above, you can use an sort method to sort it out. 
+//To find the corresponding number, you can use and indexOf method that is provided by the c#; 
+
+
+
+// numbers.Sort();
+// foreach(var number in numbers){
+//     Console.WriteLine(number);
+// }
--- Part 9 - Arrays, Lists, Indexing, and Foreach/obj/Debug/net8.0/Part 9 - Arrays, Lists, Indexing, and Foreach.AssemblyInfo.cs
+++ Part 9 - Arrays, Lists, Indexing, and Foreach/obj/Debug/net8.0/Part 9 - Arrays, Lists, Indexing, and Foreach.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Part 9 - Arrays, Lists, Indexing, and Foreach")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+fbf1994ade7c604ef5b844b9ed93c7e6982122ef")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+e3545118cdb98c13cbf284da393a4e35f6519eb8")]
 [assembly: System.Reflection.AssemblyProductAttribute("Part 9 - Arrays, Lists, Indexing, and Foreach")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Part 9 - Arrays, Lists, Indexing, and Foreach")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
